fa7aa7b4bb620d781ac14b94fb630ba1f5976fd9
ianrtracey@gmail.com <ianrtracey@gmail.com>
2015-08-06 23:07:29 -0700

    Beginning of id indexing

---
+++ db/migrate/20150807060717_add_id_to_models.rb
@@ -0,0 +1,4 @@
+class AddIdToModels < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807055208) do
+ActiveRecord::Schema.define(version: 20150807060324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20150807055208) do
   create_table "items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "type",       null: false
+    t.integer  "category",   null: false
     t.text     "content",    null: false
   end
 
